e0c50202579d430e422ffff4d2fe4892f803112b
george michael faust <georgemichaelfaust@gmail.com>
2016-03-08 19:20:01 -0500

    dependencies

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import binToHex from 'bin-to-hex';
 import binToDecimal from 'bin-to-decimal';
+import decToHex from 'dec-to-hex';
 
 const binToHex = bin => decToHex( binToDecimal(bin) );
 module.exports = binToHex;
